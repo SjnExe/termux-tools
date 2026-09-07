d72f3adc927fbbff14ab3dfc47c246d8c97c1bd2
SjnExe <muhammedshajahan100@gmail.com>
2026-09-07 22:18:31 +0530

    feat(completions): add completions for bash, zsh, and fish

---
+++ completions/fish/chsh.fish
@@ -0,0 +1,37 @@
+complete -c chsh -f
+complete -c chsh -s h -l help -d "Show usage information"
+
+function __fish_chsh_shells
+    set -l known_shells \
+        ash \
+        bash \
+        beanshell \
+        bsh \
+        cicada \
+        csh \
+        dash \
+        eltclsh \
+        elvish \
+        etsh \
+        fish \
+        hilbish \
+        ksh \
+        loksh \
+        mksh \
+        nu \
+        pwsh \
+        rc \
+        sh \
+        tcsh \
+        tsh \
+        xonsh \
+        zsh
+
+    for sh in $known_shells
+        if type -q $sh
+            echo $sh
+        end
+    end
+end
+
+complete -c chsh -s s -x -a "(__fish_chsh_shells)" -d "Login shell"
